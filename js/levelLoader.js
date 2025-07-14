78d487c3c7b125322d52b45bade9ca9c5a0d22a4
Camilla Soares Sousa <camillasoares818@gmail.com>
2025-07-14 11:30:49 -0300

    feat> carrega level 4

--- js/levelLoader.js
+++ js/levelLoader.js
@@ -7,5 +7,7 @@ document.querySelectorAll(".faseBtn").forEach(button => {
 
         if (fase === "1") iniciarFase1();
         else if (fase === "2") iniciarFase2();
+        else if (fase === "3") iniciarFase3();
+        else if (fase === "4") iniciarFase4();
     });
 });
